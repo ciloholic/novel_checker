b61d6f01cb2711c92fc478a4157603e1ac51e886
ciloholic <ciloholic0119@gmail.com>
2020-04-26 14:18:44 +0900

    change cron schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 set :job_template, "/bin/bash -l -i -c ':job'"
 
-hhmm = %w(00 01 02 19 20 21 22 23).map { |hh| "#{hh}:#{rand(1...59).to_s.rjust(2, '0')}" }
+hhmm = [*0..23].map { |hh| "#{hh.to_s.rjust(2, '0')}:#{rand(1...59).to_s.rjust(2, '0')}" }
 every 1.day, at: hhmm do
   rake 'novel_scraping:all_site'
 end
