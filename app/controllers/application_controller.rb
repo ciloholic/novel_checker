cf33ca57d5fb281b4cba75100def552e3aac1e11
ciloholic <ciloholic0119@gmail.com>
2022-03-29 22:49:48 +0900

    fix routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  private
+
+  def render404
+    @error = { status: 404, message: t('server_errors.defaults.status404') }
+    render status: :not_found, template: 'errors/error'
+  end
+
+  def render500
+    @error = { status: 500, message: t('server_errors.defaults.status500') }
+    render status: :internal_server_error, template: 'errors/error'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,13 @@ Rails.application.routes.draw do
     resources :novels
     resources :chapters
     resources :scraping_statuses
+
+    root to: 'sites#index'
   end
 
   get ':code/:novel_id', to: 'site#index', code: /[a-z0-9\-]+/, novel_id: /[0-9a-f\-]+/
   get ':code/:novel_id/:chapter_id', to: 'site#show', code: /[a-z0-9\-]+/, novel_id: /[0-9a-f\-]+/, chapter_id: /[0-9a-f\-]+/
 
   root to: 'site#top'
-  match '*any' => 'application#render_404', via: :all
+  match '*any' => 'application#render404', via: :all
 end
