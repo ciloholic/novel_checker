2e150b3edab3f32740bca6c340a8610be37c4a49
ciloholic <ciloholic0119@gmail.com>
2023-01-08 00:37:04 +0900

    remove job_template

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,8 +2,6 @@
 
 require File.expand_path('../config/environment', __dir__)
 
-set :job_template, "ash -il -c ':job'"
-
 hhmm = [*0..23].map { |hh| "#{hh.to_s.rjust(2, '0')}:#{rand(1...59).to_s.rjust(2, '0')}" }
 if Time.zone.now.day == 1
   hhmm.pop
