3f9f2b779dec2cd8947f39e26dea545b0538340b
ciloholic <ciloholic0119@gmail.com>
2026-02-08 17:42:02 +0900

    fix: update Gemfile.lock and yarn.lock for dependency upgrades; enhance development.rb logging configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,11 +42,17 @@ Rails.application.configure do
   # Set localhost to be used by links generated in mailer templates.
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  # Output logs to STDOUT.
+  config.logger = ActiveSupport::TaggedLogging.logger($stdout)
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
+
+  # Suppress ActiveRecord query logs from stdout.
+  config.active_record.logger = nil
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
